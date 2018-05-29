029722795f6332f5b9f7c905425c18a5bf7c7081
Marc Coury <marccoury@ingresso.co.uk>
2018-05-29 14:30:02 +0100

    fixed status call

--- example/main.go
+++ example/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	log "github.com/sirupsen/logrus"
 
 	ticketswitch "github.com/ingresso-group/goticketswitch.v2"
@@ -14,12 +16,14 @@ func main() {
 	}
 	//params.CostRange = true
 	results, err := client.GetAvailability("7AA-5", &params)
+	fmt.Println("\n\nAVAILABILITY:")
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("%+v", results)
 
 	sources, err := client.GetSources(nil)
+	fmt.Println("\n\nSOURCES:")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,8 +37,24 @@ func main() {
 		Seats:          []string{"A1", "A2"},
 	}
 	reservation, err := client.MakeReservation(reserveParams)
+	fmt.Println("\n\nRESERVATION:")
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("%+v", reservation)
+
+	transactionParams := ticketswitch.TransactionParams{TransactionUUID: reservation.Trolley.TransactionUUID}
+	status, err := client.GetStatus(&transactionParams)
+	fmt.Println("\n\nSTATUS:")
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Printf("%+v", status)
+
+	success, err := client.ReleaseReservation(&transactionParams)
+	fmt.Println("\n\nRELEASE:")
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Printf("%+v", success)
 }
