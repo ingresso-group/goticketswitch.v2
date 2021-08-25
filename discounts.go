995f974a70f5f837592bc2204215ceab362dba0b
Joseph <88033574+joegrimeringresso@users.noreply.github.com>
2021-08-25 11:47:39 +0100

    Minmaxage (#22)
    * Add support for tsw-session-track-id
    
    * Move to private + helper methods to handle context
    
    * changelog
    
    * use x-request-id instead
    
    * this is half a test to see if a non existant field breaks it
    
    * int not str
    
    * formatting
    
    Co-authored-by: Devan Patel <devan2patel@gmail.com>
    Co-authored-by: Lee Stennett <leestennett@ingresso.co.uk>
    Co-authored-by: Joseph.Grimer <joseph.grimer@ingresso.co.uk>
    Co-authored-by: Daniel Golding <dan.golding@ingresso.co.uk>

--- discounts.go
+++ discounts.go
@@ -8,14 +8,18 @@ type Discount struct {
 	AllowsLeavingSingleSeats string          `json:"allows_leaving_single_seats"`
 	Code                     string          `json:"discount_code"`
 	Description              string          `json:"discount_desc"`
+	MinimumEligibleAge       int             `json:"discount_minimum_eligible_age"`
+	MaximumEligibleAge       int             `json:"discount_maximum_eligible_age"`
 	IsOffer                  bool            `json:"is_offer"`
 	NonOfferSeatprice        decimal.Decimal `json:"non_offer_sale_seatprice"`
 	NonOfferSurcharge        decimal.Decimal `json:"non_offer_sale_surcharge"`
+	NonOfferCombined         decimal.Decimal `json:"non_offer_sale_combined"`
 	NumberAvailable          int             `json:"number_available"`
 	PercentageSaving         decimal.Decimal `json:"percentage_saving"`
 	PriceBandCode            string          `json:"price_band_code"`
 	Seatprice                decimal.Decimal `json:"sale_seatprice"`
 	Surcharge                decimal.Decimal `json:"sale_surcharge"`
+	Combined                 decimal.Decimal `json:"sale_combined"`
 }
 
 // DiscountsHolder is the intermediary discounts holder -- an artefact of the API
